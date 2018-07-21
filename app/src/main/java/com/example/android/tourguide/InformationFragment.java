b3951b495b5065f271c8611c1832d39de78dba46
MarcGallegos <tech.theta@yahoo.com>
2018-07-21 16:51:55 -0600

    90% commit

--- app/src/main/java/com/example/android/tourguide/InformationFragment.java
+++ app/src/main/java/com/example/android/tourguide/InformationFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,12 +19,9 @@ import java.util.ArrayList;
  */
 public class InformationFragment extends Fragment {
 
-
     public InformationFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,25 +29,44 @@ public class InformationFragment extends Fragment {
 
         //Creates GuideWord ArrayList for this fragment
         final ArrayList<GuideWord> location = new ArrayList<GuideWord>();
-        location.add(new GuideWord("Palisade Wineries List","https://visitpalisade.com/wineries-and-spirits/ -Reservations Recommended",R.drawable.wineries));
-        location.add(new GuideWord("Randy's Southside Diner", "2430 North Avenue", R.drawable.randys_ssd));
-        location.add(new GuideWord("Main Street Cafe", "504 Main Street", R.drawable.main_street_cafe));
-        location.add(new GuideWord("Pufferbelly Station Restaurant", "337 South 1st Street", R.drawable.pufferbelly));
-        location.add(new GuideWord("626 on Rood Modern Cuisine and Winebar", "2430 North Avenue", R.drawable.six_two_six));
-        location.add(new GuideWord("Il Bistro Italiano", "400 Main Street", R.drawable.il_bistro_italiano));
-        location.add(new GuideWord("Bin 707 Foodbar", "225 North 5th Street", R.drawable.bin_seven_o_seven));
-        location.add(new GuideWord("Ginger Oriental Restaurant", "2381 Patterson Road", R.drawable.ginger));
-        location.add(new GuideWord("Rib City Grill", "2830 North Avenue", R.drawable.rib_city));
-        location.add(new GuideWord("Junct'n Square Pizza", "119 North 7th Street", R.drawable.junct_n_square));
-        location.add(new GuideWord("Food Dudes Delivery Service", "fooddudesdelivery.com/grandjunction", R.drawable.food_dudes));
-        location.add(new GuideWord("Grub Gofers Delivery Service", "www.grubgofers.com/", R.drawable.grub_gofers));
+        location.add(new GuideWord(
+                "https://coloweedery.com/learn/",
+                "Responsible Consumption of Cannabis:\nIMPORTANT guidelines regarding " +
+                        "responsible and legally compliant\ncannabis possession and consumption as" +
+                        " a guest in our state\nCourtesy of Colorado Weedery"));
+        location.add(new GuideWord(
+                "https://www.tumbleweed420.com/location/parachute",
+                "10 things Colorado Tourists need to know" +
+                        "\n about Dispensary Etiquette"));
+        location.add(new GuideWord(
+                "https://www.outtherecolorado.com/gallery/17-things-know-visiting-colorado/",
+                "17 things you need to know before visiting Colorado"));
+        location.add(new GuideWord(
+                "https://countryjam.com",
+                "Country Jam Information"));
+        location.add(new GuideWord(
+                "https://www.western-co.com/rock-jam-colorado/",
+                "Rock Jam"));
+        location.add(new GuideWord(
+                "https://www.visitgrandjunction.com/events/festivals-fairs",
+                "Grand Junction Events and Festival Information"));
+        location.add(new GuideWord(
+                "https://www.visitgrandjunction.com/hiking-trails",
+                "Grand Junction Hiking Trails"));
+        location.add(new GuideWord(
+                "https://mix1043fm.com/free-camping-grand-junction/",
+                "Free Camping In Grand Junction" +
+                        "(Please refer to 17 Things You Need To Know.. list item above"));
+        location.add(new GuideWord(
+                "https://cpw.state.co.us",
+                "Colorado Parks & Wildlife\nHunting & Fishing Licenses & Information"));
 
         // Creates an {@link ArrayAdapter}, whose data source is a list of Strings. The
         // adapter knows how to create layouts for each item in the list, using the
         // simple_list_item_1.xml layout resource defined in the Android framework.
         // This list item layout contains a single {@link TextView}, which the adapter will
         // set to display a single word.
-        GuideWordAdapter adapter = new GuideWordAdapter(getActivity(), location, R.color.category_dining);
+        GuideWordAdapter adapter = new GuideWordAdapter(getActivity(), location, R.color.category_information);
 
         // Finds the {@link ListView} object in the view heirarchy of the {@link Activity}.
         // There is be a  {@link ListView} with the view id named list which is declared in
@@ -61,13 +78,17 @@ public class InformationFragment extends Fragment {
         // This is done by calling the setAdapter method on the {@link ListView} object and pass in 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
         listView.setAdapter(adapter);
 
+        //Sets OnItemClickListener on list_view to monitor item selection
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                //Logs word status currently present in index position for this activity
-                Log.v("Information Fragment", "Current location:" + location);
-            }
 
+                //Gets item selected index position
+                GuideWord guideword = location.get(position);
+
+                //Shows location status currently present in selected index position for this activity via Toast message
+                Toast.makeText(getContext(),"List Item Selected:"+ guideword , Toast.LENGTH_LONG).show();
+            }
         });
         return rootView;
     }
